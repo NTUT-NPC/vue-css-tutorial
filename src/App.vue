4320e72c5befb745f734a9c740eeba362f2bacfe
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-12-17 19:55:29 +0800

    feat: HTML structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,55 @@
 <script setup>
+import "https://kit.fontawesome.com/f4ab973a80.js"
 </script>
 
 <template>
   <main>
-    <h1>Riley's Website</h1>
+    <h1>Chenghsuan Ho</h1>
+
+    <h2>About me</h2>
+    <img src="https://picsum.photos/200" alt="My profile picture">
+    <p>Born in 2003, I'm probably the worst programmer the world has seen since then.</p>
+    <ul>
+      <li>
+        <i class="fa-solid fa-cake-candles"></i>
+        <span>12 May, 2003</span>
+      </li>
+      <li>
+        <i class="fa-solid fa-school"></i>
+        <span>Taipei Tech, Electronics</span>
+      </li>
+      <li>
+        <i class="fa-solid fa-suitcase"></i>
+        <span>Lightenform</span>
+      </li>
+    </ul>
+
+    <h2>Projects</h2>
+    <ul>
+      <li>
+        <h3>libITC</h3>
+        <p>VHDL library for the 108th to 112nd ITC hardware</p>
+      </li>
+      <li>
+        <h3>Emily</h3>
+        <p>Discord.js assistant for the NTUT Programming Club. Originally written in Java.</p>
+      </li>
+      <li>
+        <h3>flash51</h3>
+        <p>A 8051 flash tool built for the Principles and Applications of Microcomputers course.</p>
+      </li>
+      <li>
+        <h3>HarmonyFlow</h3>
+        <p>Cross-platform, responsive ChatGPT front end written in Flutter.</p>
+      </li>
+    </ul>
+
+    <h2>Socials</h2>
+    <ul>
+      <li><a href="https://www.facebook.com/chenghsuandottxt/">Facebook</a></li>
+      <li><a href="https://www.instagram.com/chenghsuan.jpg/">Instagram</a></li>
+      <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Twitter</a></li>
+    </ul>
   </main>
 </template>
 
